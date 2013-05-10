dc4284e2a72e2f9e8e827af4bbba95a549e716fa
Henri Tremblay <henri.tremblay@gmail.com>
2013-05-10 21:50:02 +0200

    Put back the full list when empty

--- cocktailfactory/webapp/src/main/webapp/js/controllers.js
+++ cocktailfactory/webapp/src/main/webapp/js/controllers.js
@@ -3,12 +3,14 @@
 /* Controllers */
 
 function CocktailListCtrl($scope, $http) {
-    $http.get('services/cocktail/list').success(function (data) {
-        $scope.cocktails = data;
-    });
-    $scope.change = function () {
-      $http.get('services/cocktail/filter/' + $scope.query).success(function (data) {
+  $http.get('services/cocktail/list').success(function (data) {
+    $scope.cocktails = data;
+  });
+  $scope.change = function () {
+    var url = ($scope.query ? ('services/cocktail/filter/' + $scope.query) : 'services/cocktail/list');
+    $http.get(url).
+      success(function (data) {
         $scope.cocktails = data;
       });
-    };
+  };
 }
