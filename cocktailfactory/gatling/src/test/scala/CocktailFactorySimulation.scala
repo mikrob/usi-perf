4de30afd3018153b91514bdcd7e127daea463746
mikrob <mrobert@octo.com>
2013-05-20 20:52:45 +0200

    changed server adress in injection script

--- cocktailfactory/gatling/src/test/scala/CocktailFactorySimulation.scala
+++ cocktailfactory/gatling/src/test/scala/CocktailFactorySimulation.scala
@@ -6,17 +6,17 @@ import bootstrap._
 import assertions._
 
 class CocktailFactorySimulation extends Simulation {
-  val port = 8180
+  val port = 80
 
   val httpConf = httpConfig
-    .baseURL("http://localhost:"+port)
+    .baseURL("http://192.168.56.5:"+port)
     .acceptHeader("application/json, text/plain, */*")
     .acceptCharsetHeader("ISO-8859-1,utf-8;q=0.7,*;q=0.3")
     .acceptEncodingHeader("gzip,deflate,sdch")
     .acceptLanguageHeader("en-US,en;q=0.8,fr;q=0.6")
     .connection("keep-alive")
     .userAgentHeader("Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.31 (KHTML, like Gecko) Chrome/26.0.1410.64 Safari/537.31")
-    .warmUp("http://localhost:"+port+"/index.html")
+    .warmUp("http://192.168.56.5:"+port+"/index.html")
 
   val headers_13 = Map(
     "Accept" -> """text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"""
